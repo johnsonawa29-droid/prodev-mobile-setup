44d4f155a4548f210bf83c9343701adff425fe3d
johnsonawa29-driod <johnsonawa29@gmail.com>
2025-11-01 15:03:46 +0100

    yes

---
+++ mobile-development-setup/app-example/app/tabs/index.tsx
@@ -0,0 +1,11 @@
+// file: app-example/app/(tabs)/index.tsx
+
+import { Text, View } from 'react-native'
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>** First App Created**</Text>
+    </View>
+  )
+}
